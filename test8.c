e1e06fc6b0bb4c3d23cb5248c69be56f5d63e9c6
ishak laaouafi <laaouafi6@gmail.com>
2025-09-03 15:18:48 +0200

    EX2.1

---
+++ test8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    int p, I, c,n,C,i;
+    i=1;
+    while ((p==978|| p==979)&&(I>0 && I<=9)&&(c>0 && c>=999)&&(n>0 && n<=99999)&&(C>0 && C<=9))
+    {
+       printf("entrer votre infos:");
+       scanf("%d %d %d %d %d", &p ,&I ,&c, &n, &C); /* code */
+       i++;
+    }
+    printf("l'ISBN:%d-%d-%d-%d-%d", p, I , c , n , C );
+    return 0;
+    
+
+}
